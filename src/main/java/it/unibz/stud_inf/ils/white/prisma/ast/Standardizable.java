1c16d54e18222304afcfa75a3f4d822204f9c2ae
Lorenz Leutgeb <lorenz.leutgeb@stud-inf.unibz.it>
2018-04-10 15:47:45 +0200

    Undeprecate standardizable

--- src/main/java/it/unibz/stud_inf/ils/white/prisma/ast/Standardizable.java
+++ src/main/java/it/unibz/stud_inf/ils/white/prisma/ast/Standardizable.java
@@ -4,8 +4,6 @@ import it.unibz.stud_inf.ils.white.prisma.Counter;
 
 import java.util.Map;
 
-@Deprecated
 public interface Standardizable<S> {
-	@Deprecated
 	S standardize(Map<Long, Long> map, Counter generator);
 }
